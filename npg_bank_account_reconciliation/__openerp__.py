38870b1c16a07bfd2f6d09a72abfd0eb1916b64e
Vincent Vinet <vincent.vinet@savoirfairelinux.com>
2014-10-30 14:27:53 -0400

    add my name in contributors for previous work, add missing dep

--- npg_bank_account_reconciliation/__openerp__.py
+++ npg_bank_account_reconciliation/__openerp__.py
@@ -93,10 +93,12 @@ Direct Journal Entries
 Contributors
 ------------
 * Mathieu Benoit (mathieu.benoit@savoirfairelinux.com)
+* Vincent Vinet (vincent.vinet@savoirfairelinux.com)
 """,
     'author': 'NovaPoint Group LLC',
     'website': ' http://www.novapointgroup.com',
     'depends': [
+        'account_cutoff_base',
         'account_voucher',
         'report_webkit',
     ],
